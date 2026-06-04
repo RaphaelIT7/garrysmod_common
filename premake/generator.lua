ee4d464d31765df1961cf6aae657b90f3bf377a5
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-06-05 01:19:22 +0200

    Fix broken project generation

--- premake/generator.lua
+++ premake/generator.lua
@@ -99,7 +99,7 @@ function CreateWorkspace(config)
 			runtime("Release")
 
 		filter("configurations:Release")
-			flags("LinkTimeOptimization")
+			LinkTimeOptimization("On")
 			optimize("Full")
 			symbols("Off")
 			defines("NDEBUG")
